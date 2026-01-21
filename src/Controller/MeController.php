9133c43df01da83e6d59049fc80f39730fbb2375
unknown <boryskosa09@gmail.com>
2026-01-21 16:05:14 +0100

    zadanie 3

--- src/Controller/MeController.php
+++ src/Controller/MeController.php
@@ -6,10 +6,6 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Core\User\UserInterface;
 
-/**
- * Zadanie (ocena 3):
- * - endpoint /api/me ma działać [dopisać roles]
- */
 final class MeController
 {
     #[Route('/api/me', name: 'api_me', methods: ['GET'])]
@@ -18,7 +14,7 @@ final class MeController
         return new JsonResponse([
             'id' => method_exists($user, 'getId') ? $user->getId() : null,
             'email' => method_exists($user, 'getEmail') ? $user->getEmail() : null,
-//            'roles' => ,
+            'roles' =>  $user->getRoles(),
         ]);
     }
 }
